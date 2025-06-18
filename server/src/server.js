250eb81c91bf6509169d3620acd251834038ff5c
Your Name <rajukunarapu49@gmail.com>
2025-06-18 20:57:25 +0530

    app.listen removed

--- server/src/server.js
+++ server/src/server.js
@@ -2,9 +2,8 @@ require('dotenv').config();
 const app = require("./app");
 const connectDB = require("./config/db")
 
+module.exports = app;
+
 connectDB().then(() => {
   console.log("DB connected successfully");
-  app.listen(`${process.env.SERVER_PORT}`, () =>
-    console.log(`Server running on ${process.env.SERVER_PORT}`)
-  );
 }).catch((error)=>console.log(error))
